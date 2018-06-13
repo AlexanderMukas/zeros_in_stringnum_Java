3f3110fa8efe3c93487077b7d901be6f300317ca
am <alexey.mukas@gmail.com>
2018-06-14 00:02:59 +0200

    - reformat - fix calc (case when 100 -> 2 (but to be 0)) - minor refactoring

--- src/main/java/com/alexander/Zeros.java
+++ src/main/java/com/alexander/Zeros.java
@@ -3,28 +3,28 @@ package com.alexander;
 import java.util.Scanner;
 
 public class Zeros {
-    public int calculating(char varArray[]){
-        int temp = 0;
-        int max_zero = 0;
+    public int calculating(char varArray[]) {
+        int result = 0;
+        int maxZeros = 0;
+
 //        System.out.println("Оно состоит из: " + varArray.length + " цифр.");
-        // Основной цикл - перебор по всем цифрам
         for (Character ch : varArray) {
 //            System.out.print("Цифра ---> " + ch);
-            if (ch == '0'){
+            if (ch == '0') {
 //                System.out.println(" <--- Это ноль!");
-                max_zero += 1;
-                if (temp < max_zero){
-                    temp = max_zero;
-                }
-
-            }else {
+                maxZeros += 1;
+            } else {
 //                System.out.println(" <--- Попался НЕ ноль :(");
-                max_zero = 0;
+                if (result < maxZeros) {
+                    result = maxZeros;
+                }
+                maxZeros = 0;
             }
         }
 
-        return temp;
+        return result;
     }
+
     public static void main(String[] args) {
         String result;
         int nZero;
@@ -34,13 +34,13 @@ public class Zeros {
         Scanner in = new Scanner(System.in);
         String varString = in.next();
 
-        System.out.println("Вы выбрали число: " + varString );
+        System.out.println("Вы выбрали число: " + varString);
         char vArr[] = varString.toCharArray();
 
         Zeros obj = new Zeros();
         // Вызываем метод для вычисления нулей
         nZero = obj.calculating(vArr);
-        result = ("Максимальное кол-во нулей между цифрами = " + nZero );
+        result = ("Максимальное кол-во нулей между цифрами = " + nZero);
 
         System.out.println(result);
 
