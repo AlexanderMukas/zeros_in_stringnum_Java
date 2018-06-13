c8623fe208033b596d5b4ad1db1f6f8b33301bdd
am <alexey.mukas@gmail.com>
2018-06-14 00:03:20 +0200

    refactor

--- src/main/java/com/alexander/Zeros.java
+++ src/main/java/com/alexander/Zeros.java
@@ -46,24 +46,3 @@ public class Zeros {
 
     }
 }
-
-/*
-
-# JMH version: 1.21
-# VM version: JDK 1.8.0_172-ea, Java HotSpot(TM) 64-Bit Server VM, 25.172-b03
-# VM invoker: D:\dev\jdk\jdk-8u172\jre\bin\java.exe
-# VM options: -javaagent:D:\dev\apps\idea-18.2\lib\idea_rt.jar=59653:D:\dev\apps\idea-18.2\bin -Dfile.encoding=UTF-8
-# Warmup: 3 iterations, 1 s each
-# Measurement: 5 iterations, 1 s each
-# Timeout: 10 min per iteration
-# Threads: 1 thread, will synchronize iterations
-# Benchmark mode: Average time, time/op
-# Benchmark: perf.ZeroCalcPerf.calculating
-
-Benchmark                                     Mode  Cnt   Score    Error   Units
-ZeroCalcPerf.calculating                      avgt    5  20.325 ±  0.104   ns/op
-ZeroCalcPerf.calculating:·gc.alloc.rate       avgt    5  ≈ 10⁻⁴           MB/sec
-ZeroCalcPerf.calculating:·gc.alloc.rate.norm  avgt    5  ≈ 10⁻⁵             B/op
-ZeroCalcPerf.calculating:·gc.count            avgt    5     ≈ 0           counts
-
- */
