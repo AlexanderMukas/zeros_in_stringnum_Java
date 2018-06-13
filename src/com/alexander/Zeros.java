8dee1e387f144e6a68c91f1f5050a30957cc6d3e
AlexanderMukas <alexandermukas@yahoo.com>
2018-06-13 20:33:39 +0300

    Update...

--- src/com/alexander/Zeros.java
+++ src/com/alexander/Zeros.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Zeros {
     public static void main(String[] args) {
+        System.out.println("Привет. Вот программа на Java");
         System.out.print("Введите тестовое число: ");
         Scanner in = new Scanner(System.in);
         System.out.println(in.next());
