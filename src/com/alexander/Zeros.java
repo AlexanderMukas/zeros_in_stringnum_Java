9f2cc825eb046f95fddb326796ac7a78ad81417b
AlexanderMukas <alexandermukas@yahoo.com>
2018-06-13 20:05:09 +0300

    Start coding...

---
+++ src/com/alexander/Zeros.java
@@ -0,0 +1,11 @@
+package com.alexander;
+
+import java.util.Scanner;
+
+public class Zeros {
+    public static void main(String[] args) {
+        System.out.print("Введите тестовое число: ");
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.next());
+    }
+}
