9bfba6fed6671533c1d5a75b47070b873b0cdd7e
AlexanderMukas <alexandermukas@yahoo.com>
2018-06-13 22:20:16 +0300

    Update 3...

--- src/com/alexander/Zeros.java
+++ src/com/alexander/Zeros.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 public class Zeros {
     public static void main(String[] args) {
         int temp = 0;
-        int number = 0;
         int max_zero = 0;
+        String result;
         System.out.print("Введите тестовое число: ");
 
         Scanner in = new Scanner(System.in);
@@ -22,12 +22,20 @@ public class Zeros {
             System.out.print("Цифра ---> " + ch);
             if (ch == '0'){
                 System.out.println(" <--- Попался ноль!");
+                max_zero += 1;      //max_zero++;
+                if (temp < max_zero){
+                    temp = max_zero;
+                }
+
             }else {
                 System.out.println(" <--- Попался НЕ ноль :(");
+                max_zero = 0;
+                //continue;
             }
 
         }
-        System.out.println("Конец расчета!");
+        result = ("Максимальное кол-во нулей между цифрами = " + temp);
+        System.out.println(result);
 
     }
 }
