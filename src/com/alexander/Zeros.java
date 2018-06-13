04e752c97194ebf738616699d0a86009c55cefe2
AlexanderMukas <alexandermukas@yahoo.com>
2018-06-13 22:08:32 +0300

    Test commit...

--- src/com/alexander/Zeros.java
+++ src/com/alexander/Zeros.java
@@ -1,18 +1,33 @@
 package com.alexander;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 import java.util.Scanner;
 
 public class Zeros {
     public static void main(String[] args) {
-        System.out.println("Привет. Вот программа на Java");
+        int temp = 0;
+        int number = 0;
+        int max_zero = 0;
         System.out.print("Введите тестовое число: ");
+
         Scanner in = new Scanner(System.in);
-        String num = in.next();
-        for (Character temp: num.toCharArray()
-             ) {
-            System.out.println("Цифра ---> " + temp);
+        String varString = in.next();
+
+        System.out.println("Вы выбрали число: " + varString );
+        char arr[] = varString.toCharArray();
+        System.out.println("Оно состоит из: " + arr.length + " цифр.");
+
+        for (Character ch : arr) {
+            System.out.print("Цифра ---> " + ch);
+            if (ch == '0'){
+                System.out.println(" <--- Попался ноль!");
+            }else {
+                System.out.println(" <--- Попался НЕ ноль :(");
+            }
 
         }
-        System.out.println("Вы ввели цифру - " + num);
+        System.out.println("Конец расчета!");
+
     }
 }
